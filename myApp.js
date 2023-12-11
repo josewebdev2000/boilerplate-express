f62079cc307cb523efa64b75c55047b4aa338223
josewebdev2000 <josewebdev@proton.me>
2023-12-11 17:10:25 -0500

    Added Log Middleware

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
 let express = require('express');
 let app = express();
-require("dotenv").config();
 
 console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.get("/json", (req, res) => {
     if (process.env.MESSAGE_STYLE == "uppercase")
