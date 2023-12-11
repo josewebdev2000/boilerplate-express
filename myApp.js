66db26dca1d0c97a5d4ef68638b5d1a2a28486be
josewebdev2000 <josewebdev@proton.me>
2023-12-11 16:30:23 -0500

    Added Express route to return JSON as a response

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ let app = express();
 console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
+app.get("/json", (req, res) => res.json({"message" : "Hello json"}));
 
 
 
