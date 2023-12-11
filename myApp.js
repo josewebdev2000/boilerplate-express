2cdceb60726d6bc1edc5bc833e21658fe66f81fa
josewebdev2000 <josewebdev@proton.me>
2023-12-11 16:44:34 -0500

    Added environment variables

--- myApp.js
+++ myApp.js
@@ -1,11 +1,20 @@
 let express = require('express');
 let app = express();
+require("dotenv").config();
 
 console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
-app.get("/json", (req, res) => res.json({"message" : "Hello json"}));
-
+app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE == "uppercase")
+    {
+        res.json({"message": "HELLO JSON"});
+    }
+    else
+    {
+        res.json({"message": "Hello json"});
+    }
+});
 
 
 
