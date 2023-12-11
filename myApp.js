ab587c341139d16059250c8b9a3c209ad91671e5
josewebdev2000 <josewebdev@proton.me>
2023-12-11 16:11:58 -0500

    Serve index.html to main route of Express App

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 console.log("Hello World");
-app.get("/", (req, res) => res.send("Hello Express"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 
 
