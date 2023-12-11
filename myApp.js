04f88b691e6befc3e3a323ed8dad0e18b5e8c79b
josewebdev2000 <josewebdev@proton.me>
2023-12-11 17:27:02 -0500

    Using Request Parameters

--- myApp.js
+++ myApp.js
@@ -3,12 +3,15 @@ let express = require('express');
 let app = express();
 
 console.log("Hello World");
+
 app.use("/public", express.static(__dirname + "/public"));
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
 });
+
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
+
 app.get("/json", (req, res) => {
     if (process.env.MESSAGE_STYLE == "uppercase")
     {
@@ -19,6 +22,7 @@ app.get("/json", (req, res) => {
         res.json({"message": "Hello json"});
     }
 });
+
 app.get(
     "/now", 
     (req, res, next) => {
@@ -29,6 +33,10 @@ app.get(
         return res.json({time: req.time});
     }
 );
+
+app.get("/:word/echo", (req, res) => {
+    return res.json({word: req.params.word});
+});
 
 
 
