feca39f4921be4fb0ef9e2bc7be1e3eeb94cabf2
josewebdev2000 <josewebdev@proton.me>
2023-12-11 16:24:27 -0500

    Serving static files through /public route

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 console.log("Hello World");
-app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 
